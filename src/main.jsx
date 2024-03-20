4c12d83412f42d634abce291d2b6e5c7f7b9591c
Eze Tochukwu <157438236+Tochukwu-Elijah@users.noreply.github.com>
2024-03-20 12:59:35 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
